d01765d88de8270558dec22765bdf3e3b9ad29a6
AnanthD21 <ande9392@colorado.edu>
2022-04-06 17:25:04 +0000

    Test package

--- base_external/package/aesd-hello-world/aesd-hello-world.mk
+++ base_external/package/aesd-hello-world/aesd-hello-world.mk
@@ -1,27 +1,25 @@
-
 ##############################################################
 #
-# AESD-ASSIGNMENTS
+# AESDHELLOWORLD
 #
 ##############################################################
 
-#TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'c0b3c81b3e15c03afa3eadf195c21d105bb01a70'
+#TODO: Fill up the contents below in order to reference final project git contents
+AESD_HELLO_WORLD_VERSION = c0b3c81b3e15c03afa3eadf195c21d105bb01a70
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/final-project-karthik-b-r.git'
-AESD_ASSIGNMENTS_SITE_METHOD = git
-AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+AESD_HELLO_WORLD_SITE = git@github.com:cu-ecen-aeld/final-project-karthik-b-r
+AESD_HELLO_WORLD_SITE_METHOD = git
+AESD_HELLO_WORLD_GIT_SUBMODULES = YES
 
-define AESD_ASSIGNMENTS_BUILD_CMDS
+define AESD_HELLO_WORLD_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/test all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
-define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
+define AESD_HELLO_WORLD_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/test/* $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
-
