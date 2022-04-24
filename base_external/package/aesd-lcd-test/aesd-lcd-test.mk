f0b4f3b2438e96e7b5066b9a9a130bc24b99ca12
karthik-b-r <kaba5545@colorado.edu>
2022-04-24 22:16:37 +0000

    LCD Formatted

--- base_external/package/aesd-lcd-test/aesd-lcd-test.mk
+++ base_external/package/aesd-lcd-test/aesd-lcd-test.mk
@@ -5,7 +5,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference final project git contents
-AESD_LCD_TEST_VERSION = 59f0b1ec5e7ab2b547f417ade11a3b831f245a34
+AESD_LCD_TEST_VERSION = 1e79060003aea25e0aac72874e13899d75dff45e
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -15,11 +15,13 @@ AESD_LCD_TEST_GIT_SUBMODULES = YES
 
 define AESD_LCD_TEST_BUILD_CMDS
         $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/LCD_check lcd
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/lcd_new server_display
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_LCD_TEST_INSTALL_TARGET_CMDS
         $(INSTALL) -m 0755 $(@D)/LCD_check/* $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/lcd_new/* $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
